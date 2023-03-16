9420296975052368709362cbec9a3621978e105e
Iustinian Litoiu <litoiuig@cardiff.ac.uk>
2023-03-16 19:29:53 +0000

    A Minecraft username checker
    First Project Completed, Fun working on it.

---
+++ Minecraft Username Checker/isavailable.py
@@ -0,0 +1,43 @@
+import requests
+import random
+import string
+import colorama
+import time
+from colorama import Fore
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate a random username.')
+parser.add_argument('-l', '--length', type=int, default=4, help='size length of the username')
+args = parser.parse_args()
+Checked_Usernames = []
+check1 = "Couldn't find any profile with that name"
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase + string.digits + "_"
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    Checked_Usernames.append(Checked_Usernames)
+    return result_str
+
+while True:
+    try:
+        username = str(get_random_string(args.length)) #Replace the number inside here for how many characters long you want the username
+        if username not in Checked_Usernames:
+            url = f'https://api.mojang.com/users/profiles/minecraft/{username}?'
+            response = requests.get(url)
+            if response.status_code == 204 or response.status_code == 404:
+                print(Fore.GREEN, "Username Available: ", username) # Needs more checks
+            elif response.status_code == 200:
+                uuid = response.json()['id']
+                print(Fore.RED, username,  "Is Unavailable!")
+            else:
+                print(Fore.MAGENTA, "Being Rate Limited Waiting.", end="\r", flush=True)
+                time.sleep(1)
+                print(Fore.MAGENTA, "Being Rate Limited Waiting..", end="\r", flush=True)
+                time.sleep(1)
+                print(Fore.MAGENTA, "Being Rate Limited Waiting...")
+        else:
+            pass
+
+    except KeyError:
+        pass

7998961af1797223439c7d9d3d59c328472d48e1
Iustinian Litoiu <litoiuig@cardiff.ac.uk>
2023-03-24 13:38:40 +0000

    Included a choose Username condition.

--- Minecraft Username Checker/isavailable.py
+++ Minecraft Username Checker/isavailable.py
@@ -8,10 +8,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Generate a random username.')
 parser.add_argument('-l', '--length', type=int, default=4, help='size length of the username')
+parser.add_argument('-u', '-U', type=str, help='Checks for a given username')
 args = parser.parse_args()
 Checked_Usernames = []
 check1 = "Couldn't find any profile with that name"
-
+check_once = False
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase + string.digits + "_"
@@ -21,15 +22,23 @@ def get_random_string(length):
 
 while True:
     try:
-        username = str(get_random_string(args.length)) #Replace the number inside here for how many characters long you want the username
+        if args.u:
+            username = args.u
+            check_once = True
+        else:
+            username = str(get_random_string(args.length)) #Replace the number inside here for how many characters long you want the username
         if username not in Checked_Usernames:
             url = f'https://api.mojang.com/users/profiles/minecraft/{username}?'
             response = requests.get(url)
             if response.status_code == 204 or response.status_code == 404:
-                print(Fore.GREEN, "Username Available: ", username) # Needs more checks
+                print(Fore.GREEN, "Username available: ", username) # Needs more checks
+                if check_once:
+                    break
             elif response.status_code == 200:
                 uuid = response.json()['id']
-                print(Fore.RED, username,  "Is Unavailable!")
+                print(Fore.RED, "Username unavailable: ", username)
+                if check_once:
+                    break
             else:
                 print(Fore.MAGENTA, "Being Rate Limited Waiting.", end="\r", flush=True)
                 time.sleep(1)
